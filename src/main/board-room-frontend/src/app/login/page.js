66dbb79b5c47e966f4640133e0a4a2293c51f0fc
Lee Sunjae <sunjae7944@naver.com>
2024-11-20 00:25:32 +0900

    svc/page 파일 수정 - 전체 폴더 수정

---
+++ src/main/board-room-frontend/src/app/login/page.js
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function page() {
+    return (
+        <div>
+            로그인 페이지
+        </div>
+    )
+}
---
+++ src/main/board-room-frontend/src/app/signup/page.js
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function signup() {
+    return (
+        <div>
+            회원가입 페이지
+        </div>
+    )
+}
